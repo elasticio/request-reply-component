d0fb8306f5d46314cbcf4ebbb773489da390937f
EmptyInfinity <soroka14021998@gmail.com>
2021-04-21 18:24:45 +0300

    try with objectStorage

--- replyWithAttachment.js
+++ replyWithAttachment.js
@@ -3,15 +3,30 @@ const {
 } = require("@elastic.io/component-commons-library");
 const { default: axios } = require("axios");
 const { messages } = require("elasticio-node");
+const Encryptor = require("elasticio-sailor-nodejs/lib/encryptor");
+const { ObjectStorage } = require("@elastic.io/object-storage-client");
 
 const JWTToken = process.env.ELASTICIO_OBJECT_STORAGE_TOKEN;
 const maesterUri = process.env.ELASTICIO_OBJECT_STORAGE_URI;
+const PASSWORD = process.env.ELASTICIO_MESSAGE_CRYPTO_PASSWORD;
+const VECTOR = process.env.ELASTICIO_MESSAGE_CRYPTO_IV;
 
 const HEADER_CONTENT_TYPE = "Content-Type";
 const HEADER_ROUTING_KEY = "X-EIO-Routing-Key";
 const DEFAULT_CONTENT_TYPE = "application/json";
 const HEADER_STATUS_CODE = "x-eio-status-code";
 const HEADER_OBJECT_STORAGE = "x-ipaas-object-storage-id";
+
+const objectStorage = new ObjectStorage({
+  uri: maesterUri,
+  jwtSecret: JWTToken,
+});
+
+const encryptor = new Encryptor(PASSWORD, VECTOR);
+objectStorage.use(
+  () => encryptor.createCipher(),
+  () => encryptor.createDecipher()
+);
 
 exports.process = async function processMessage(msg) {
   try {
@@ -31,17 +46,18 @@ exports.process = async function processMessage(msg) {
       responseUrl,
       "stream"
     );
-    console.log(result.data);
 
-    const { objectId } = await sendStreamToStorage(
-      result.data,
-      maesterUri,
-      JWTToken
-    );
+    const objectId = await objectStorage.addAsStream(result.data);
+
+    // const { objectId } = await sendStreamToStorage(
+    //   result.data,
+    //   maesterUri,
+    //   JWTToken
+    // );
 
     const reply = messages.newMessageWithBody({});
     reply.headers[HEADER_ROUTING_KEY] = replyTo;
-    reply.headers[HEADER_CONTENT_TYPE] = "application/pdf";
+    reply.headers[HEADER_CONTENT_TYPE] = "image/png";
     reply.headers[HEADER_OBJECT_STORAGE] = objectId;
 
     if (msg.body.customHeaders) {
