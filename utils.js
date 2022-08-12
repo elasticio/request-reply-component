2213c3542fb0b6a5c01e53b9c8d49010f4a810de
EmptyInfinity <soroka14021998@gmail.com>
2022-08-12 11:44:58 +0300

    update

--- utils.js
+++ utils.js
@@ -1,9 +1,9 @@
 const packageJson = require('./package.json');
 const compJson = require('./component.json');
 
-exports.getUserAgent = (lib) => {
+exports.getUserAgent = (libName) => {
   const { name: compName } = packageJson;
   const { version: compVersion } = compJson;
-  const libVersion = packageJson.dependencies['@elastic.io/maester-client'];
-  return `${compName}/${compVersion} ${lib}/${libVersion}`;
+  const libVersion = packageJson.dependencies[`@elastic.io/${libName}`];
+  return `${compName}/${compVersion} ${libName}/${libVersion}`;
 };
